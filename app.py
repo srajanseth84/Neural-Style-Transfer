08f208045b83d2259532a0e78b24533364951720
Srajan Seth <srajanseth84@gmail.com>
2021-09-15 17:51:44 +0530

    Add emoji to predict button

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ st.set_page_config(page_title="Neural Style Transfer",
 ''' # Neural Style Transfer'''
 
 st.write('Neural Style Transfer is a technique that uses deep learning to compose one image in the style of another image. Have your ever wished you could paint like Picasso or Van Gogh? This is your chance! \n')
-st.write("To know more about this app, visit [**GitHub**](https://github.com/srajanseth84/Neural-Style-Transfer)")
+st.write(
+    "To know more about this app, visit [**GitHub**](https://github.com/srajanseth84/Neural-Style-Transfer)")
 
 
 @st.cache
@@ -68,21 +69,22 @@ with style_image:
     st.write('## Style Image...')
     chosen_style = st.radio(
         ' ',
-        ("Choose from List" , "Upload", "URL"))
+        ("Choose from List", "Upload", "URL"))
     if chosen_style == 'Choose from List':
         try:
-        # Delete style.jpg before setting
+            # Delete style.jpg before setting
             os.remove("style.jpg")
         except:
             pass
         st.write(f"You choosed to Select from List!")
-        select = st.selectbox('List', ('Starry night','Clocks','Picasso Portret','Mona Lisa','The Kiss by Klimt','Birth of Venus','Church in Auvers','Sejalec','The Scream','Kofetarica',))
+        select = st.selectbox('List', ('Starry night', 'Clocks', 'Picasso Portret', 'Mona Lisa',
+                              'The Kiss by Klimt', 'Birth of Venus', 'Church in Auvers', 'Sejalec', 'The Scream', 'Kofetarica',))
 
         if select == 'Starry night':
             url = 'https://media.overstockart.com/optimized/cache/data/product_images/VG485-1000x1000.jpg'
-        elif select =='Clocks':
+        elif select == 'Clocks':
             url = 'https://upload.wikimedia.org/wikipedia/en/d/dd/The_Persistence_of_Memory.jpg'
-        elif select =='Picasso Portret':
+        elif select == 'Picasso Portret':
             url = 'https://images.saatchiart.com/saatchi/1311333/art/6500245/5569923-AOAGHVQR-7.jpg'
         elif select == 'Mona Lisa':
             url = 'https://cdn.cnn.com/cnnnext/dam/assets/190430171751-mona-lisa.jpg'
@@ -92,7 +94,7 @@ with style_image:
             url = 'https://art-sheep.com/wp-content/uploads/2019/06/Sandro-Botticelli-Birth-of-Venus-1024x683.jpg'
         elif select == 'Church in Auvers':
             url = 'https://cdn.theculturetrip.com/wp-content/uploads/2019/01/vincent_van_gogh_-_the_church_in_auvers-sur-oise_view_from_the_chevet_-_google_art_project.jpg'
-        elif select =='The Scream':
+        elif select == 'The Scream':
             url = 'https://upload.wikimedia.org/wikipedia/commons/9/9d/The_Scream_by_Edvard_Munch%2C_1893_-_Nasjonalgalleriet.png'
         elif select == 'Kofetarica':
             url = 'https://upload.wikimedia.org/wikipedia/commons/6/61/Ivana_Kobilca_-_Kofetarica.jpg'
@@ -138,7 +140,7 @@ with style_image:
     except:
         pass
 
-predict = st.button('Start Neural Style Transfer...')
+predict = st.button('Transfering Style 🖼')
 
 
 if predict:
